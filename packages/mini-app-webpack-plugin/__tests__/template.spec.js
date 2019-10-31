f3b37956dd5e5cda95f0a6a0f3c80bed9e4cb242
luckyadam <weitaozsh@gmail.com>
2019-10-31 14:08:05 +0800

    feat(taro): mini-runner 支持新框架编译

--- packages/mini-app-webpack-plugin/__tests__/template.spec.js
+++
@@ -1,9 +0,0 @@
-import { buildTemplates } from '../src/template/index'
-
-describe('template', () => {
-  test('test', () => {
-    expect(() => buildTemplates(5)).not.toThrowError()
-
-    expect(false).toBeTruthy()
-  })
-})
